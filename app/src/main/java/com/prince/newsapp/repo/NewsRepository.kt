07bb6f6079133c2e8d2629f70694efc625e38ae4
chrysophilist <100132795+chrysophilist@users.noreply.github.com>
2025-09-24 00:52:38 +0530

    Implement repository to abstract API calls

---
+++ app/src/main/java/com/prince/newsapp/repo/NewsRepository.kt
@@ -0,0 +1,23 @@
+package com.prince.newsapp.repo
+
+import com.prince.newsapp.models.NewsResponse
+import com.prince.newsapp.network.NewsApiService
+import retrofit2.Response
+import javax.inject.Inject
+import com.prince.newsapp.BuildConfig
+
+class NewsRepository @Inject constructor(
+    private val api: NewsApiService
+) {
+
+    private val apiKey = BuildConfig.API_KEY
+
+    suspend fun getTopHeadlines(
+        country: String = "us",
+        category: String = "business"
+    ): Response<NewsResponse> {
+
+        if (apiKey.isBlank()) throw IllegalStateException("API Key not found.")
+        return api.getTopHeadlines(country, category, apiKey)
+    }
+}
